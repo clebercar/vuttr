ca0458bebd74d506cd099ff1d6bbe24c7b88f13e
cleber.carvalho <contatoclbercarvalho@gmail.com>
2019-04-28 22:35:58 -0300

    Muda estrutura do controller para fazer uso da estrutura de classes

--- controllers/ToolController.js
+++ controllers/ToolController.js
@@ -1,15 +1,41 @@
 const Tool = require('../models/Tool')
 
-module.exports = {
-    async index(req, res){
-        const tools = await Tool.find({}).sort('-createAt')
+class ToolController{
+    async index(req, res){ 
+        let tools = await Tool
+            .find({})
+            .select('id title link description tags')
+            .sort('-createAt')
+
+        if(req.query.tag)
+            tools = tools.filter((el) => el.tags.indexOf(req.query.tag) > -1)
 
         return res.json(tools)
-    },
+    }
 
     async store(req, res){
         const tool = await Tool.create(req.body)
 
         return res.json(tool)
-    },
+    }
+
+    async show(req, res){
+        const tool = await Tool.findById(req.params.id)
+
+        return res.json(tool)
+    }
+
+    async destroy(req, res){
+        const id = req.params.id
+
+        await Tool.deleteOne({_id: id})
+            .then((tool) => {
+                res.status(200).json(tool);
+            }, function (error) {
+                console.log(error);
+                res.sendStatus(500);
+            })
+    }
 }
+
+module.exports = new ToolController
