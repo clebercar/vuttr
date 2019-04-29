1a25402cd2a3c1964f907969dbc39abef5f047c6
cleber.carvalho <contatoclbercarvalho@gmail.com>
2019-04-28 22:37:10 -0300

    Adiciona rotas show e delete para o controller de ferramentas

--- routes.js
+++ routes.js
@@ -4,7 +4,9 @@ const routes = express.Router()
 
 const ToolController = require('./controllers/ToolController')
 
-routes.get('/tools', ToolController.index)
+routes.get('/tools', ToolController.index.bind(ToolController))
+routes.get('/tools/:id', ToolController.show)
 routes.post('/tools', ToolController.store)
+routes.delete('/tools/:id', ToolController.destroy);
 
 module.exports = routes
