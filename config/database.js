8c3fc1e721df75f290bf2a00ba729ba9759a0325
cleber.carvalho <contatoclbercarvalho@gmail.com>
2019-04-30 01:37:19 -0300

    Ajusta arquivo de configuração do banco para fazer uso do dotenv

--- config/database.js
+++ config/database.js
@@ -1,10 +1,13 @@
-module.exports = (uri) => {
-    const mongoose = require('mongoose')
+require('dotenv').config()
+const mongoose = require('mongoose')
 
-    mongoose.connect(`mongodb://${uri}`,{
-        useNewUrlParser: true
-    })
-}
+const DB_USER = process.env.DB_USER
+const DB_PASS = process.env.DB_PASS
 
+const uri = `mongodb://${DB_USER}:${DB_PASS}@ds147446.mlab.com:47446/vuttr`
 
+mongoose.connect(uri, {
+  useNewUrlParser: true
+})
 
+module.exports = mongoose
