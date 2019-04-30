e5090afd5bf8e266c5f9a6a7c53db454afc166dc
cleber.carvalho <contatoclbercarvalho@gmail.com>
2019-04-30 01:40:28 -0300

    Ajusta código para fazer uso do eslint

--- models/Tool.js
+++ models/Tool.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 
 const ToolSchema = new mongoose.Schema({
-    title: String,
-    link: String,
-    description: String,
-    tags: [String],
-    createAt: {
-        type: Date,
-        default: Date.now,
-    },
+  title: String,
+  link: String,
+  description: String,
+  tags: [String],
+  createAt: {
+    type: Date,
+    default: Date.now
+  }
 })
 
 module.exports = mongoose.model('Tool', ToolSchema)
