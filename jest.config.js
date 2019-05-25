341e5f63855d50bff9b5c6f4f7fa0ae152febbb6
cleber.carvalho <contatoclbercarvalho@gmail.com>
2019-05-25 03:04:01 -0300

    Configuração do Jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
 module.exports = {
+  verbose: true,
+  setupFilesAfterEnv: [
+    '<rootDir>/__tests__/setup.js'
+  ],
   bail: true,
   clearMocks: true,
+  collectCoverage: true,
+  coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/__tests__/'],
+  collectCoverageFrom: ['**/*.{js}'],
+  coverageDirectory: '__tests__/coverage',
   testEnvironment: 'node',
   testMatch: [
     '**/__tests__/**/*.test.js?(x)'
