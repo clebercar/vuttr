8f3bd8edebcf78d5ace4392ed1169a64967b6db9
cleber.carvalho <contatoclbercarvalho@gmail.com>
2019-05-25 02:57:02 -0300

    Ajusta model para não salva o password do usuário sem que seja criptografado

--- models/User.js
+++ models/User.js
@@ -1,31 +1,40 @@
-const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
-const User = new mongoose.Schema({
+const User = new db.Schema({
   name: String,
   email: String,
-  password: String,
-  password_hash: String,
-  createAt: {
-    type: Date,
-    default: Date.now
+  password_hash: String
+}, {
+  timestamps: {
+    createdAt: 'created_at'
   }
 })
 
+User.virtual('password')
+  .get(function () {
+    return this.__password
+  })
+  .set(function (password) {
+    this.__password = password
+  })
+
 User.pre('save', async function (next) {
-  this.password_hash = await bcrypt.hash(this.password, 8)
+  this.password_hash = await bcrypt.hashSync(this.__password, 8)
   next()
 })
 
-User.post('save', () => mongoose.disconnect())
+User.methods.checkPassword = function (password) {
+  return bcrypt.compareSync(password, this.password_hash, (err, res) => {
+    if (res) return true
 
-User.methods.checkPassword = (password) => {
-  return bcrypt.compare(password, this.password_hash)
+    console.log(err)
+    return false
+  })
 }
 
-User.methods.generateToken = (candidatePassword, cb) => {
+User.methods.generateToken = function () {
   return jwt.sign({ id: this.id }, process.env.APP_SECRET)
 }
 
-module.exports = mongoose.model('User', User)
+module.exports = db.model('User', User)
