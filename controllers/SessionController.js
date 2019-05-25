8b0a7f661c6a50f7eae8222d3d3ac2953d7b4579
cleber.carvalho <contatoclbercarvalho@gmail.com>
2019-05-25 02:53:24 -0300

    Definição do processo de autenticação

--- controllers/SessionController.js
+++ controllers/SessionController.js
@@ -1,6 +1,23 @@
+const User = require('../models/User')
+
 class SessionController {
   async store (req, res) {
-    return res.sendStatus(200)
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+
+    if (!user.checkPassword(password)) {
+      return res.status(401).json({ message: 'Incorret password' })
+    }
+
+    if (!user) {
+      return res.status(401).json({ message: 'User not found' })
+    }
+
+    return res.json({
+      user,
+      token: user.generateToken()
+    })
   }
 }
 
