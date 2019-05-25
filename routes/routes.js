bba176386c285366a63dae603cd4d4aabff857ea
cleber.carvalho <contatoclbercarvalho@gmail.com>
2019-05-25 02:57:48 -0300

    Muda a forma de criação de rotas para fazer uso do middleware

--- routes/routes.js
+++ routes/routes.js
@@ -1,24 +1,24 @@
+const authMiddleware = require('../middlewares/auth')
 const ToolController = require('../controllers/ToolController')
 const UserController = require('../controllers/UserController')
 const SessionController = require('../controllers/SessionController')
 
-module.exports = app => {
-  app.route('/tools')
-    .get(ToolController.index.bind(ToolController))
-    .post(ToolController.store.bind(ToolController))
+const routes = require('express').Router()
 
-  app.route('/tools/:id')
-    .get(ToolController.show.bind(ToolController))
-    .delete(ToolController.destroy.bind(ToolController))
+routes.post('/sessions', SessionController.store.bind(SessionController))
 
-  app.route('/users')
-    .get(UserController.index.bind(UserController))
-    .post(UserController.store.bind(UserController))
+routes.use(authMiddleware)
 
-  app.route('/users/:id')
-    .get(UserController.show.bind(UserController))
-    .delete(UserController.destroy.bind(UserController))
+routes.get('/tools', ToolController.index.bind(ToolController))
+routes.post('/tools', ToolController.store.bind(ToolController))
 
-  app.route('/sessions')
-    .post(SessionController.store.bind(SessionController))
-}
+routes.get('/tools/:id', ToolController.show.bind(ToolController))
+routes.delete('/tools/:id', ToolController.destroy.bind(ToolController))
+
+routes.get('/users', UserController.index.bind(UserController))
+routes.post('/users', UserController.store.bind(UserController))
+
+routes.get('/users/:id', UserController.show.bind(UserController))
+routes.delete('/users/:id', UserController.destroy.bind(UserController))
+
+module.exports = routes
