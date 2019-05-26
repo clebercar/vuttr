46e7c5aed09e096e0d24fa72bd5f831994fd793c
cleber.carvalho <contatoclbercarvalho@gmail.com>
2019-05-26 00:18:41 -0300

    Complementa o teste unitário da model User

--- __tests__/unit/user.test.js
+++ __tests__/unit/user.test.js
@@ -1,14 +1,62 @@
+const factory = require('../factories')
+const faker = require('faker')
 const User = require('../../models/User')
 
 describe('User', () => {
-  it('should encrypt user password', async () => {
-    const user = await User.create({
-      name: 'Peter',
-      email: 'peter@gmail.com',
-      password: '123456'
+  let user
+
+  describe('CREATE', () => {
+    beforeEach(async () => {
+      user = await factory.create('User')
     })
 
-    let checkPassword = user.checkPassword(user.password)
-    expect(checkPassword).toBe(true)
+    it('should create a user', async () => {
+      const createdUser = await User.findOne({ name: user.name })
+
+      expect(user._id).toEqual(createdUser._id)
+    })
+
+    it('should encrypt password whenever a user is created', async () => {
+      const checkPassword = user.checkPassword(user.password)
+
+      expect(checkPassword).toBe(true)
+    })
+  })
+
+  describe('UPDATE', () => {
+    beforeEach(async () => {
+      user = await factory.create('User')
+    })
+
+    it('should update name of user', async () => {
+      const userUpdate = await user.updateOne({ name: faker.name.findName() })
+
+      expect(userUpdate.name).not.toBe(user.name)
+    })
+  })
+
+  describe('DELETE', () => {
+    beforeEach(async () => {
+      user = await factory.create('User')
+    })
+
+    it('should delete a user', async () => {
+      const userDelete = await user.deleteOne({ id: user._id })
+      const userExist = await User.findOne({ id: userDelete._id })
+
+      expect(userExist).toBe(null)
+    })
+  })
+
+  describe('READ', () => {
+    beforeEach(async () => {
+      await factory.createMany('User', 50)
+    })
+
+    it('should return all users created', async () => {
+      const allUsers = await User.find({})
+
+      expect(allUsers.length).toEqual(50)
+    })
   })
 })
