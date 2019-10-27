f932ec47aa3b9ae156d4f68af2954a69613862e5
clebercar <contatoclebercarvalho@gmail.com>
2019-10-26 22:19:50 -0300

    Adiciona configuração inicial de CORS

--- config/index.js
+++ config/index.js
@@ -1,8 +1,10 @@
 require('./database')
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(require('../routes/routes'))
 
