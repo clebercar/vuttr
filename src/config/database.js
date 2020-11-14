b9ce58167594cc8b6c5241702686ec4aece82285
clebercar <contatoclebercarvalho@gmail.com>
2020-11-14 02:15:04 -0300

    refactor: change local to string connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,23 @@
+const dotenv = require('dotenv')
 const mongoose = require('mongoose')
-const uri = require('./database-uri')
+
+dotenv.config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+})
+
+const DB_HOST = process.env.DB_HOST
+const DB_PORT = process.env.DB_PORT
+const DB_NAME = process.env.DB_NAME
+
+const uri = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}).then(result => {
-  console.log('MongoDB Conectado', uri)
+}).then(() => {
+  console.log('🐵 mongoDB connected 🐵', uri)
 }).catch(error => {
-  console.log(error)
+  console.error(error)
 })
 
 module.exports = uri
